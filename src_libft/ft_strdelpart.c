10fe3e9c25191e169b1184267e44f67c9f05b519
AnastasBa <anastasba@gmail.com>
2017-03-17 22:32:58 -0700

    adding files

--- src_libft/ft_strdelpart.c
+++ src_libft/ft_strdelpart.c
@@ -4,9 +4,9 @@ void ft_strdelpart(char **str, size_t start)
 {
 	if (*str!=NULL && start < ft_strlen(*str))
 	{
-		while (str[start] != '\0')
+		while ((*str)[start] != '\0')
 		{
-			str[start] = '\0';
+			(*str)[start] = '\0';
 			start++;
 		}
 	}
